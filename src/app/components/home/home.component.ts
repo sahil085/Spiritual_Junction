a53d9ffb21a5e2549a480eefcb5400022e501bb8
Ravi Garg <ravi.garg@tothenew.com>
2020-05-25 11:58:55 +0530

    added activities section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  activities = [{
+    name: 'CAMPS',
+    image: 'https://iyfdelhi.com/wp-content/uploads/2013/06/Nature-51.jpg',
+    action_btn: 'KNOW MORE',
+    action_btn_link: '',
+  }, {
+    name: 'SCHOOL PROGRAMS',
+    image: 'https://iyfdelhi.com/wp-content/uploads/2013/06/kids-leaving-schoolpbr.jpg',
+    action_btn: 'KNOW MORE',
+    action_btn_link: '',
+  }, {
+    name: 'COLLEGE PROGRAMS',
+    image: 'https://iyfdelhi.com/wp-content/uploads/2013/06/shutterstock_314450846.jpg',
+    action_btn: 'KNOW MORE',
+    action_btn_link: '',
+  }, {
+    name: 'YOUTH FESTS',
+    image: 'https://iyfdelhi.com/wp-content/uploads/2013/06/shutterstock_612941954.jpg',
+    action_btn: 'KNOW MORE',
+    action_btn_link: '',
+  }];
+
   constructor() { }
 
   ngOnInit() {
