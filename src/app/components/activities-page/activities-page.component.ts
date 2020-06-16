f2083563d473644fb8e08a750469451785b19dff
Ravi Garg <ravi.garg@tothenew.com>
2020-06-16 19:07:34 +0530

    added mixins and constants

--- src/app/components/activities-page/activities-page.component.ts
+++ src/app/components/activities-page/activities-page.component.ts
@@ -16,17 +16,19 @@ export class ActivitiesPageComponent implements OnInit {
   }, {
     name: 'SCHOOL PROGRAMS',
     image: 'https://iyfdelhi.com/wp-content/uploads/2013/06/kids-leaving-schoolpbr.jpg',
-    description: '',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     action_btn: 'KNOW MORE',
     action_btn_link: '',
   }, {
     name: 'COLLEGE PROGRAMS',
     image: 'https://iyfdelhi.com/wp-content/uploads/2013/06/shutterstock_314450846.jpg',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     action_btn: 'KNOW MORE',
     action_btn_link: '',
   }, {
     name: 'YOUTH FESTS',
     image: 'https://iyfdelhi.com/wp-content/uploads/2013/06/shutterstock_612941954.jpg',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
     action_btn: 'KNOW MORE',
     action_btn_link: '',
   }];
