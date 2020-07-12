6e30be35cd7bf91729c3f52a6f17467c3f54afd5
Ravi Garg <ravi.garg@tothenew.com>
2020-07-12 14:15:29 +0530

    styling fixes

--- src/app/components/festivals-page/festivals-page.component.ts
+++ src/app/components/festivals-page/festivals-page.component.ts
@@ -11,19 +11,19 @@ export class FestivalsPageComponent implements OnInit {
   festivals = [{
     name: 'UDGAAR',
     desc: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book.',
-    image: '',
+    image: 'https://iyfdelhi.com/wp-content/uploads/2018/07/2.jpg',
     inner_link: '',
     outer_link: 'https://udgaar.in'
   }, {
     name: 'VIPLAVA',
     desc: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book.',
-    image: '',
+    image: 'https://iyfdelhi.com/wp-content/uploads/2018/07/2.jpg',
     inner_link: '',
     outer_link: 'https://www.facebook.com/pg/IYFGhaziabad/photos/?tab=album&album_id=164008645128374'
   }, {
     name: 'Mega Youth Fest',
     desc: 'Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero\'s De Finibus Bonorum et Malorum for use in a type specimen book.',
-    image: '',
+    image: 'https://iyfdelhi.com/wp-content/uploads/2018/07/2.jpg',
     inner_link: '',
     outer_link: 'https://www.facebook.com/pg/IYFGhaziabad/photos/?tab=album&album_id=128262368703002'
   }];
